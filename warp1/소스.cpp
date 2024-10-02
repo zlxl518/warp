306615bd451fde36fa7cd4172fe86daf7bce5d89
zlxl518 <zlxl51848@gmail.com>
2024-10-02 14:39:11 +0900

    클래스 완성

--- warp1/소스.cpp
+++ warp1/소스.cpp
@@ -1,51 +1,25 @@
 #include <iostream>
 using namespace std;
-void board_setup(int(*board)[24]);
-void board_print(int(*board)[24]);
+
+class Board
+{
+public: // 외부에서도 사용 가능
+	int board[12][24];
+	void Board_Setup(int(*board)[24]);
+	void Board_Print(int(*board)[24]);
+};
 
 int main() {
-	/*int board[12][24] = {0};
-	
-	for (int i = 0; i < 24; ++i) {
-		for (int j = 0; j < 12; ++j) {
-			if ((i == 0) || (i == 23)) {
-				board[j][i] = 1;
-			}
-			else if ((j == 0) || (j == 11)) {
-				board[j][i] = 1;
-			}
-			else board[j][i] = 0;
-		}
-	}//여기까지가 board 테두리 초기화
-
-	int inputx, inputy;
-	while (1) {
-		cin >> inputx >> inputy;
-		board[inputx][inputy] = 1;
-		for (int y = 0; y < 24; ++y) {
-			for (int x = 0; x < 12; ++x) {
-				if (board[x][y] == 1)
-				{
-					cout << "□";
-				}
-				else cout << "  ";
-			}
-			cout << endl;
-		}
-	}//좌표 입력 및 전체 출력
-	*/
-
-	//함수 호출 구상
+	Board Board1;
 	int board[12][24] = { 0 };
-	board_setup(board);
+	Board1.Board_Setup(board);
 	while (1) {
-		board_print(board);
+		Board1.Board_Print(board);
 	}
 	return 0;
 }
 
-void board_setup(int (*board)[24]) {
-	//board[12][24] = { 0 };
+void Board::Board_Setup(int (*board)[24]) {
 	for (int y = 0; y < 24; y++)
 	{	
 		for (int x = 0; x < 12; x++)
@@ -55,7 +29,7 @@ void board_setup(int (*board)[24]) {
 	}
 }
 
-void board_print(int (*board)[24]) {
+void Board::Board_Print(int (*board)[24]) {
 	int inputx, inputy;
 	cin >> inputx >> inputy;
 	board[inputx][inputy] = 1;
